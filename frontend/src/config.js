369791acf4c38e618358433fd157db29a108ac47
opcpower <109979133+opcpower@users.noreply.github.com>
2022-07-26 16:56:03 +0300

    Update config.js
    somechanges

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   chainId: 56,
-  contractAddress: "0x630a827e0784ab4ff2c1b8aC13b8139d8BFEE2e0",
+  contractAddress: "0xF0aA6e6052b0674E9b17672b3CC31D85a119d6d0",
   scanLink:
-    "https://www.bscscan.com/address/0x630a827e0784ab4ff2c1b8aC13b8139d8BFEE2e0"
+    "https://www.bscscan.com/address/0xF0aA6e6052b0674E9b17672b3CC31D85a119d6d0"
 };
